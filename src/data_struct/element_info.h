41e98a5c43917ca950791f8b97f61f8978dd80db
Arthur Glowacki <aglowacki.anl@gmail.com>
2026-06-18 15:27:04 -0500

    revert from contexpr to cont because of compiler errors on MSVC

--- src/data_struct/element_info.h
+++ src/data_struct/element_info.h
@@ -155,7 +155,7 @@ private:
 };
    
 // placeholder so H starts at 1
-inline constexpr std::string Element_Symbols[] = {" ", "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl",
+inline const std::string Element_Symbols[] = {" ", "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl",
 						                        "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As",
 				                                "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In",
 												"Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb",
