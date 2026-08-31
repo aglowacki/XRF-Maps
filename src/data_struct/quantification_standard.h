63251d439ecc79adcf7ef07990797658102f9939
Arthur Glowacki <aglowakci@anl.gov>
2026-08-31 11:33:14 -0500

    Fixed segfault if we are loading from detector we should not delete

--- src/data_struct/quantification_standard.h
+++ src/data_struct/quantification_standard.h
@@ -177,7 +177,7 @@ public:
     std::string standard_filename;
     std::unordered_map<std::string, T_real> element_standard_weights;
 
-    // element name       cts
+    // fit routine                                        element name       cts
     std::unordered_map<Fitting_Routines, std::unordered_map<std::string, T_real> > element_counts;
 
     Spectra<T_real> integrated_spectra;
